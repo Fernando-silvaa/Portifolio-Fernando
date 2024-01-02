055f48467c191ecf247319a87f3ec1e1a76cf9fa
“Fernando <nando394pd.6@gmail.com>
2024-01-02 20:45:28 -0300

    Mudando código arquivo main

--- js/main.js
+++ js/main.js
@@ -9,34 +9,45 @@ const git = document.getElementById('git');
 const gitHub = document.getElementById('gitHub');
 
 
+const tecnologias = {
+    1: 'HTML é uma linguagem de marcação utilizada na construção de páginas na Web.',
+    2: 'CSS é um mecanismo para adicionar estilos a uma página web, aplicado diretamente nas tags HTML',
+    3: 'SASS é uma linguagem de folha de estilo que é compilada para CSS. Ele permite que você use variáveis, regras aninhadas, mixins, funções e muito mais, tudo com uma sintaxe totalmente compatível com CSS',
+    4: 'O Bootstrap é um framework CSS baseado na estilização de componentes através de classes diminuindo o tempo de execução de desenvolvimento web.',
+    5: 'JavaScript é uma linguagem de programação para manipulação de elementos e muito utilizada para páginas web dinâmicas.',
+    6: 'TypeScript é uma linguagem de programação de forte tipagem para manipulação de elementos.',
+    7: 'Git é um sistema de controle de versões muito usado com o GitHub, usado principalmente no desenvolvimento de software.',
+    8: 'GitHub é uma plataforma de hospedagem de código-fonte e arquivos com controle de versão usando o Git.',
+}
+
 html.onclick = () => {
-    textoTecnologias.innerHTML = 'HTML é uma linguagem de marcação utilizada na construção de páginas na Web.'
-};
+    textoTecnologias.innerHTML = tecnologias[1]
+}
 
 css.onclick = () => {
-    textoTecnologias.innerHTML = 'CSS é um mecanismo para adicionar estilos a uma página web, aplicado diretamente nas tags HTML'
-};
+    textoTecnologias.innerHTML = tecnologias[2]
+}
 
 sass.onclick = () => {
-    textoTecnologias.innerHTML = 'SASS é uma linguagem de folha de estilo que é compilada para CSS. Ele permite que você use variáveis, regras aninhadas, mixins, funções e muito mais, tudo com uma sintaxe totalmente compatível com CSS'
+    textoTecnologias.innerHTML = tecnologias[3]
 }
 
 bootstrap.onclick = () => {
-    textoTecnologias.innerHTML = 'O Bootstrap é um framework CSS baseado na estilização de componentes através de classes diminuindo o tempo de execução de desenvolvimento web.'
+    textoTecnologias.innerHTML = tecnologias[4]
 };
 
 javaScript.onclick = () => {
-    textoTecnologias.innerHTML = 'JavaScript é uma linguagem de programação para manipulação de elementos e muito utilizada para páginas web dinâmicas.'
+    textoTecnologias.innerHTML = tecnologias[5]
 };
 
 typeScript.onclick = () => {
-    textoTecnologias.innerHTML = 'TypeScript é uma linguagem de programação de forte tipagem para manipulação de elementos.'
+    textoTecnologias.innerHTML = tecnologias[6]
 };
 
 git.onclick = () => {
-    textoTecnologias.innerHTML = 'Git é um sistema de controle de versões muito usado com o GitHub, usado principalmente no desenvolvimento de software.'
+    textoTecnologias.innerHTML = tecnologias[7]
 };
 
 gitHub.onclick = () => {
-    textoTecnologias.innerHTML = 'GitHub é uma plataforma de hospedagem de código-fonte e arquivos com controle de versão usando o Git.'
+    textoTecnologias.innerHTML = tecnologias[8]
 };
